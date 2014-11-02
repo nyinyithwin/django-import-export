1b08afd9c40d89e67a4d87421d0cacb6f4026d2e
nyinyithwin <thwin@zension.com>
2014-11-03 04:41:32 +0800

    Update admin.py

--- import_export/admin.py
+++ import_export/admin.py
@@ -114,17 +114,7 @@ class ImportMixin(ImportExportMixinBase):
         '''
         opts = self.model._meta
         resource = self.get_import_resource_class()()
-
         confirm_form = ConfirmImportForm(request.POST)
-        import_formats = self.get_import_formats()
-        input_format = import_formats[
-            int(confirm_form.cleaned_data['input_format'])
-        ]()
-        import_file_name = os.path.join(
-            tempfile.gettempdir(),
-            confirm_form.cleaned_data['import_file_name']
-        )
-        import_file = open(import_file_name, input_format.get_read_mode())
         def tableprocess(msg):
             data = import_file.read()
             if not input_format.is_binary() and self.from_encoding:
@@ -153,6 +143,15 @@ class ImportMixin(ImportExportMixinBase):
                 )
             
         if confirm_form.is_valid():
+            import_formats = self.get_import_formats()
+            input_format = import_formats[
+                int(confirm_form.cleaned_data['input_format'])
+            ]()
+            import_file_name = os.path.join(
+                tempfile.gettempdir(),
+                confirm_form.cleaned_data['import_file_name']
+            )
+            import_file = open(import_file_name, input_format.get_read_mode())
             queue = django_rq.get_queue('high')
             msg = "msg"
             queue.enqueue(tableprocess, msg)
