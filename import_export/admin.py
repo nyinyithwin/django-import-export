4ee3f50a65a2e34afcbbd2e6b30e0e82b0a6ad7c
nyinyithwin <thwin@zension.com>
2014-11-03 04:04:42 +0800

    Update admin.py

--- import_export/admin.py
+++ import_export/admin.py
@@ -14,8 +14,25 @@ from django.contrib.contenttypes.models import ContentType
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 import django_rq
+import_file_name_2 = ""
 
-def tableprocess(result):
+def tableprocess(input_format):
+    import_file = open(import_file_name_2, input_format.get_read_mode())
+    data = import_file.read()
+    if not input_format.is_binary() and "utf-8":
+        data = force_text(data, "utf-8")
+    dataset = input_format.create_dataset(data)
+    
+    result = resource.import_data(dataset, dry_run=False,
+                        raise_errors=True)
+
+            # Add imported objects to LogEntry
+    logentry_map = {
+        RowResult.IMPORT_TYPE_NEW: ADDITION,
+        RowResult.IMPORT_TYPE_UPDATE: CHANGE,
+         RowResult.IMPORT_TYPE_DELETE: DELETION,
+    }
+    content_type_id=ContentType.objects.get_for_model(self.model).pk
     for row in result:
         if row.import_type != row.IMPORT_TYPE_SKIP:
             LogEntry.objects.log_action(
@@ -134,24 +151,11 @@ class ImportMixin(ImportExportMixinBase):
                 tempfile.gettempdir(),
                 confirm_form.cleaned_data['import_file_name']
             )
-            import_file = open(import_file_name, input_format.get_read_mode())
-            data = import_file.read()
-            if not input_format.is_binary() and self.from_encoding:
-                data = force_text(data, self.from_encoding)
-            dataset = input_format.create_dataset(data)
-
-            result = resource.import_data(dataset, dry_run=False,
-                                 raise_errors=True)
-
-            # Add imported objects to LogEntry
-            logentry_map = {
-                RowResult.IMPORT_TYPE_NEW: ADDITION,
-                RowResult.IMPORT_TYPE_UPDATE: CHANGE,
-                RowResult.IMPORT_TYPE_DELETE: DELETION,
-            }
-            content_type_id=ContentType.objects.get_for_model(self.model).pk
+            import_file_name_2 = import_file_name
             queue = django_rq.get_queue('high')
-            queue.enqueue(tableprocess, result)
+            queue.enqueue(tableprocess, input_format)
+            
+            
             success_message = _('Import finished')
             messages.success(request, success_message)
             import_file.close()
