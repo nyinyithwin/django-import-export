12156667d7f759e4267d073228989564dda80103
nyinyithwin <thwin@zension.com>
2014-11-05 10:58:51 +0800

    Update admin.py

--- import_export/admin.py
+++ import_export/admin.py
@@ -3,7 +3,6 @@ from __future__ import with_statement
 import tempfile
 from datetime import datetime
 import os.path
-from racepackapp import views
 from django.contrib import admin
 from django.utils.translation import ugettext_lazy as _
 from django.conf.urls import patterns, url
@@ -78,7 +77,7 @@ class ImportMixin(ImportExportMixinBase):
                 self.admin_site.admin_view(self.process_import),
                 name='%s_%s_process_import' % info),
             url(r'^import/$',
-                self.admin_site.admin_view(views.import_csv),
+                self.admin_site.admin_view(self.import_action),
                 name='%s_%s_import' % info),
         )
         return my_urls + urls
